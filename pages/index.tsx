d09d43ebe0bb2e2a599b5cd99a55d9cfbd033d69
Juan-Camilo-Tabarquino <juancatabarquino@gmail.com>
2022-10-22 14:06:48 -0500

    Implementacion NextUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import { Button } from '@nextui-org/react';
 
 const Home: NextPage = () => {
   return (
    <>
-    <h1>Hola mundo</h1>
+    <Button color="gradient">
+      Hola mundo
+    </Button>
    </>
   )
 }
