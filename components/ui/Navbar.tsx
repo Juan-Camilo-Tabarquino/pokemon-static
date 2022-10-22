8d5e4a0285440b89ca8049463e72e927193818ad
Juan-Camilo-Tabarquino <juancatabarquino@gmail.com>
2022-10-22 17:14:34 -0500

    Avances

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,4 +1,5 @@
 import { useTheme, Text, Spacer } from '@nextui-org/react'
+import Image from 'next/image'
 
 export const Navbar = () => {
 
@@ -14,6 +15,12 @@ export const Navbar = () => {
         padding:'0px 20px',
         backgroundColor: theme?.colors.gray100.value
     }}>
+        <Image 
+           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+           alt="Icono de la app"
+           width={70}
+           height={70}
+        />
         <Text color="white" h2>P</Text>
         <Text color="white" h3>okemon</Text>
         <Spacer css={{ flex: 1 }}/>
